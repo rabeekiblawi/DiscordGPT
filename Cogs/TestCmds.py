b6e3782cb7eb785c1d42b9f22a7a512068497320
Kappa63 <kareem.qiblawi@gmail.com>
2023-04-26 01:23:05 +0300

    makeshift db

--- Cogs/TestCmds.py
+++ Cogs/TestCmds.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
-import openai
 from dotenv import load_dotenv
+import openai
+import json
 import os
 
 load_dotenv()
@@ -16,20 +17,32 @@ class TestCmds(commands.Cog):
     @commands.command(name="hello")
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def AskGPT(self, ctx, *args):
-        print("meow")
         await ctx.message.channel.send("Hello World!")
 
     @commands.command(name="ask")
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def AskGPT(self, ctx, *args):
         prompt = " ".join(args)
-        print(prompt)
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
-        )
-        print(response)
-        answer = response.choices[0].message.content.strip()
-        await ctx.message.channel.send(answer)
+        newMessage = [{"role": "user", "content": prompt}]
+        # print(ctx.message.author.id)
+        userDB = open(f"./Makeshift-DB/{ctx.message.author.id}.json", "r+")
+        try:
+            chat = json.load(userDB) + newMessage
+        except:
+            chat = newMessage
+        
+        # print(chat)
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=chat
+            )
+            # print(response)
+            answer = response.choices[0].message.content.strip()
+            await ctx.message.channel.send(answer)
+            chat += [response.choices[0].message]
+            userDB.seek(0)
+            json.dump(chat, userDB, indent=4)
+            userDB.close()
+        except Exception as e:
+            await ctx.send(f"An error occurred: {e}")
---
+++ Test.py
@@ -0,0 +1,5 @@
+import os
+import json
+
+UsrsDBs = open("Makeshift-DB/443986051371892746.json","r+")
+print(json.load(UsrsDBs)+[{"jo":"ASd"}])
