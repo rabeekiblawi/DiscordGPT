f15782d2439a0b81f07a78c15d4a57423c0f5636
rabeekiblawi <rabeekiblawi@gmail.com>
2023-05-04 19:20:32 +0300

    added slash commands

--- DiscordGPT.py
+++ DiscordGPT.py
@@ -2,61 +2,42 @@ import discord
 from discord.ext import commands
 import openai
 import os
+from discord import Intents
+from discord import app_commands
+from config import TOKEN, OPENAI_API_KEY  # Add this line to import tokens
 
-TOKEN = 'MTA5OTM2NDMyMzI3MDM0ODk1NA.G94_Lf.8OsNdDHJE70m4EDDnI1WMXfejFE_NxwCfEevio'
-openai.api_key = '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'
-
-
-intents = discord.Intents.default()
+openai.api_key = OPENAI_API_KEY
+intents = Intents.default()
 intents.typing = False
-intents.presences = False
+intents.presences = True
+intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = commands.Bot(command_prefix='d!', intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user.name} is online!')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello, World!')
-
-
-@bot.command()
-async def ask(ctx, *, question: str):
+    print("Bot is Up and Ready!")
     try:
-        prompt = f"{question}\n\nAnswer:"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        answer = response.choices[0].text.strip()
-        await ctx.send(answer)
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
     except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-
-bot.run(TOKEN)
+        print(e)
 
 
-@bot.command()
-async def ask(ctx, *, question: str):
-    try:
-        prompt = f"{question}\n\nAnswer:"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        answer = response.choices[0].text.strip()
-        await ctx.send(answer)
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
+@client.command(aliases=['p'])
+async def ping(ctx):
+    await ctx.send('pong')
 
-ß
+
+@client.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        f"Hey {interaction.user.mention}! This is a slash command!", ephemeral=True
+    )
+
+@client.tree.command(name="say")
+@app_commands.describe(things_to_say="What should I say?")
+async def say(interaction: discord.Interaction, things_to_say: str):
+    await interaction.response.send_message(f"{interaction.user.name} said: '{things_to_say}'")
+
+client.run(TOKEN)
